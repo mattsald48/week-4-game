101496aac1b3286f981b31d88780ee6c7b29881f
mattsald48 <matt.saldana8@gmail.com>
2017-07-10 14:19:38 -0500

    added in reset buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,9 @@ var x = "";
 var y = "";
 var click = 0;
 var numWins = 0;
-var opReady = false;
+var opReady = false; //returns true when there is an opponent to attack
+var resetBoo = false; // returns true when there is a reset button present
+var reset = "";       //var for reset button
 
 //assigning value to id of cards
 $("#darthMaul").data("stats", {currentHP: 120, charAP: 8, currentMultiplyer: 1, playerName: "Darth Maul"});
@@ -53,17 +55,14 @@ function defenseMovement(){
 	  	$("p:first").addClass("css-typing");
 	  	opReady = true;
 	  	click++;
-	  	
 	  	}
 		return y;
    });
-
-
 }
 	
 
-cardMovement();
-defenseMovement();
+ cardMovement();
+ defenseMovement();
  
  $("#attackButt").on("click", function(){
    if ($(x).data("stats").currentHP > 0 && opReady == true) {	
@@ -73,18 +72,22 @@ defenseMovement();
 	   $(x).children("p").html($(x).data("stats").currentHP);     //updates player card with current hp
 	   $(y).children("p").html($(y).data("stats").currentHP);     //updates enemy card  with current hp
 	   $(".playerChoice").children("p").html("You attacked " + $(y).data("stats").playerName + " for " + $(x).data("stats").charAP * $(x).data("stats").currentMultiplyer + " damage. Your damage is now multiplyed by " + $(x).data("stats").currentMultiplyer + "!");
+	   $("p:first").addClass("css-typing");
 	   $(x).data("stats").currentMultiplyer++;  				   //increases multiplyer
 	   //console.log($(x).data("stats").currentHP);
 	   
 
-	  if ($(y).data("stats").currentHP <= 0){   //hides opponent card if hp goes below zero
+	  if ($(y).data("stats").currentHP <= 0){   //hides opponent card if hp goes below zero also logs win
      	   $(y).hide();
      	   click = 0;
      	   numWins++;
      	   //console.log(numWins);
-     	   if(numWins == 3){
-     	   	 alert("You have defeated all opponents. Click ok to try again.");  //if there are no opponents game is over
-   		     location.reload();
+     	   if(numWins == 3){ //win scenario reset button is created to start over
+   		      opReady = false;
+   		      click++;
+   		      $(".playerChoice").children("p").html("You have defeted all opponents! Click reset to try again.");
+   		      resetButt();
+     	   
      	   }else{
      	    $(".playerChoice").children("p").html("Pick another challenger.")   //pick another opponent
      	    opReady = false;
@@ -95,17 +98,25 @@ defenseMovement();
  	
    } 
 
-   if ($(x).data("stats").currentHP <= 0){ //if your hp falls below zero you lose and an alert pops up
-   		alert("You have fallen in battle. Refresh the page to try again.");
-   		location.reload();
+//losing scenario section
+   if ($(x).data("stats").currentHP <= 0 && click <= 1){ //if your hp falls below zero reset button function is run.  Click keeps function from creating multiple reset buttons	
+     	 click++;
+     	 $(".playerChoice").children("p").html("You have been defeated in battle! Click reset to try again.")	 
+         resetButt();   
    }
-
-
+   
  });//end of attacButt click
 
 
  	
-
+function resetButt(){
+	console.log(click)
+	reset = $('<input type="button" value="Reset"/>');
+    $(".defenderSec").append(reset);
+	$("input").on("click", function(){
+    location.reload();
+    });
+}
 
 
 
